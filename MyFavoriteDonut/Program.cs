b9e6813363314b2a0728b6672e27024bff16f752
Christos Piliafas <piliaf1@ad.nmc.edu>
2019-01-16 10:25:33 -0500

    Add project files.

---
+++ MyFavoriteDonut/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFavoriteDonut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My favorite donut is an Apple Fritter!");
+            Console.ReadKey();
+        }
+    }
+}
